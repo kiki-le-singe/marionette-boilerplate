b6413919d060ba993fd993d1d0ee6ff1f560bfb8
kiki le singe <aalbertini95@gmail.com>
2015-05-24 18:52:45 +0200

    feat(foundation): implements foundation

--- app/scripts/init.js
+++ app/scripts/init.js
@@ -2,10 +2,16 @@ require.config({
 
   baseUrl: '/scripts',
 
-  /* starting point for application */
+  // starting point for application
   deps: ['index'],
 
   shim: {
+    foundation: {
+      deps: ['jquery']
+    },
+    'foundation.topbar': {
+      deps: ['foundation']
+    },
     backbone: {
       deps: ['underscore', 'jquery']
     },
@@ -22,21 +28,26 @@ require.config({
     underscore: '../bower_components/lodash/lodash',
     backbone: '../bower_components/backbone/backbone',
 
-    /* alias all marionette libs */
+    // alias all marionette libs
     marionette: '../bower_components/marionette/lib/core/backbone.marionette',
     'backbone.wreqr': '../bower_components/backbone.wreqr/lib/backbone.wreqr',
     'backbone.radio': '../bower_components/backbone.radio/build/backbone.radio',
     'backbone.babysitter': '../bower_components/backbone.babysitter/lib/backbone.babysitter',
 
-    /* Alias text.js for template loading and shortcut the templates dir to tmpl */
+    // Alias text.js for template loading and shortcut the templates dir to tmpl
     text: '../bower_components/requirejs-text/text',
 
-    /* Helpers */
+    // Helpers
     helpersHBS: 'helpers/helpers',
 
-    /* requirejs-plugins */
+    // requirejs-plugins
     json: '../bower_components/requirejs-plugins/src/json',
 
-    handlebars: '../bower_components/handlebars/handlebars.runtime'
+    // handlebars
+    handlebars: '../bower_components/handlebars/handlebars.runtime',
+
+    // Foundation
+    foundation: '../bower_components/foundation/js/foundation',
+    'foundation.topbar': '../bower_components/foundation/js/foundation/foundation.topbar'
   }
 });
