3a91b6358f330a84db546627215a915aa107da19
kiki le singe <aalbertini95@gmail.com>
2015-05-24 22:41:32 +0200

    feat(sass.fonts): implements fonts:dist task

--- gulp/tasks/sass/config.js
+++ gulp/tasks/sass/config.js
@@ -1,14 +1,29 @@
 'use strict';
 
 var sassDir = 'app/styles/scss';
+var foundationIconFontsDir = 'app/bower_components/foundation-icon-fonts/';
 
 module.exports = {
   sass: {
-    cssFramework: 'app/bower_components/bootstrap/dist/css/bootstrap.css',
     cssDir: 'app/styles/css',
     mainSassFile: sassDir + '/main.scss',
     dist: {
       dest: 'dist/styles/'
+    }
+  },
+  fonts: {
+    src: [
+      foundationIconFontsDir + '*.eot',
+      foundationIconFontsDir + '*.svg',
+      foundationIconFontsDir + '*.ttf',
+      foundationIconFontsDir + '*.woff'
+    ],
+    dev: {
+      path: '../../bower_components/foundation-icon-fonts'
+    },
+    dist: {
+      path: 'fonts',
+      dest: 'dist/styles/fonts/'
     }
   }
 };
