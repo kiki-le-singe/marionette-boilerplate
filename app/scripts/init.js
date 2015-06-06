a4f87c293a4fe0e963ed3116db3d588a5a8940ea
kiki le singe <aalbertini95@gmail.com>
2015-06-06 15:12:50 +0200

    refactor(init): update require paths

--- app/scripts/init.js
+++ app/scripts/init.js
@@ -29,29 +29,17 @@ require.config({
     jquery: '../bower_components/jquery/dist/jquery',
     underscore: '../bower_components/lodash/lodash',
     backbone: '../bower_components/backbone/backbone',
-
-    // alias all marionette libs
     marionette: '../bower_components/marionette/lib/core/backbone.marionette',
     'backbone.wreqr': '../bower_components/backbone.wreqr/lib/backbone.wreqr',
     'backbone.radio': '../bower_components/backbone.radio/build/backbone.radio',
     'backbone.babysitter': '../bower_components/backbone.babysitter/lib/backbone.babysitter',
-
-    // Alias text.js for template loading and shortcut the templates dir to tmpl
     text: '../bower_components/requirejs-text/text',
-
-    // Helpers
-    helpersHBS: 'helpers/helpers',
-
-    // requirejs-plugins
+    helpers: 'helpers',
+    helpersHBS: 'helpers/handlebars',
     json: '../bower_components/requirejs-plugins/src/json',
-
-    // handlebars
     handlebars: '../bower_components/handlebars/handlebars.runtime',
-
-    // Foundation
     foundation: '../bower_components/foundation/js/foundation',
     'foundation.topbar': '../bower_components/foundation/js/foundation/foundation.topbar',
-
-    bluebird: '../bower_components/bluebird/js/browser/bluebird',
+    bluebird: '../bower_components/bluebird/js/browser/bluebird'
   }
 });
